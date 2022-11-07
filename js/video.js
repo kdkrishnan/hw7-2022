383b8dd9d5a69accda366ab63c358056a7df4ac3
kdkrishnan <112428471+kdkrishnan@users.noreply.github.com>
2022-11-07 08:57:42 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -3,7 +3,8 @@ var video = document.querySelector("#player1");
 //page load
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	video.loop = false
+	video.autoPlay = false
 });
 
 //play button
@@ -21,14 +22,14 @@ document.querySelector("#pause").addEventListener("click", function() {
 //slow down
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slow Video");
-   	video.playbackRate = video.playbackRate * .90
+   	video.playbackRate *= .90
 	console.log("Playback Rate: " + video.playbackRate)
 });
 
 //speed up
 document.querySelector("#faster").addEventListener("click", function() {
 	console.log("Speed Up Video");
-   	video.playbackRate = video.playbackRate * 1.10
+   	video.playbackRate *= 1.10
 	console.log("Playback Rate: " + video.playbackRate)
 });
 
@@ -47,26 +48,34 @@ document.querySelector("#skip").addEventListener("click", function() {
 //mute
 document.querySelector("#mute").addEventListener("click", function() {
 	if (video.muted == false){
-		console.log("Mute Video")
-		video.muted = true
+		console.log("Mute Video");
+		video.muted = true;
+		mute.textContent = "Unmute"
 	}
 	else{
 		console.log("Unmute Video")
 		video.muted = false
+		mute.textContent = "Mute"
 	}
 });
 
 //volume slider
 document.querySelector("#slider").addEventListener("click", function() {
-	
+	console.log("Current Volume: ", video.volume);
+	video.volume = this.value / 100;
+	console.log("Volume: ", video.volume);
+});
+
+document.querySelector("#volume").addEventListener("click", function() {
+	video.volume * 100 + "%"
 });
 
 //styled
 document.querySelector("#vintage").addEventListener("click", function() {
-	
+	video.classList.add(oldSchool)
 });
 
 //original
 document.querySelector("#orig").addEventListener("click", function() {
-	
+	video.classList.remove(oldSchool)
 });
